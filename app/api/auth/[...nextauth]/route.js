384e651be3d0dc9a31581b0e23353f660b6b55f5
sujalkyal <sujalkyal2704@gmail.com>
2025-03-07 13:08:03 +0530

    cors removed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,21 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "../../../lib/auth";
-import { NextResponse } from "next/server";
 
-const handler = async (req) => {
-  const response = NextResponse.next();
-
-  response.headers.set("Access-Control-Allow-Credentials", "true");
-  response.headers.set("Access-Control-Allow-Origin", "*"); // Adjust for security
-  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // Handle preflight OPTIONS request
-  if (req.method === "OPTIONS") {
-    return new NextResponse(null, { status: 200 });
-  }
-
-  return NextAuth(req, response, authOptions);
-};
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
